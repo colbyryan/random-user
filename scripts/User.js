5f21851265c2b69ede90c7f139931d37ac5fc4c2
Colby Ryan <colbyr.ryan@gmail.com>
2021-09-07 15:02:37 -0500

    Added button to generate a new random user. Also added styling

--- scripts/User.js
+++ scripts/User.js
@@ -1,14 +1,19 @@
 export const User = (userObj) => {
-    return `<section class ="">
-    <h3>User Information</h3>
+    const dateObjBirth = new Date(userObj.dob.date);
+    const formattedDateBirth = dateObjBirth.toLocaleDateString();
+    const dateObjReg = new Date(userObj.registered.date);
+    const formattedDateReg= dateObjReg.toLocaleDateString();
+    return `<section class ="user-section">
+    <h3>User Information: </h3>
     <ul>
-    <li>Name: ${userObj.name.first +" "+ userObj.name.last}</li>
-    <li>Gender: ${userObj.gender}</li>
-    <li>Address: ${userObj.location.street + ", " + userObj.location.city + ", " + userObj.location.postcode }</li>
-    <li>Email Address: ${userObj.email}</li>
-    <li>Date of Birth: ${userObj.dob.date}(Age: ${userObj.dob.age})</li>
-    <li>Cell Phone Number: ${userObj.cell}</li>
-    <li>Photo: ${userObj.picture.medium}</li>
+        <li><img src="${userObj.picture.medium}"></li>
+        <li><b>Name:</b> ${userObj.name.first +" "+ userObj.name.last}</li>
+        <li><b>Gender:</b> ${userObj.gender}</li>
+        <li><b>Address:</b> ${userObj.location.street.number + " " + userObj.location.street.name + ", " + userObj.location.city + ", " + userObj.location.postcode }</li>
+        <li><b>Email Address:</b> ${userObj.email}</li>
+        <li><b>Date of Birth:</b> ${formattedDateBirth + " "}(Age: ${userObj.dob.age})</li>
+        <li><b>Date Registered:</b> ${formattedDateReg}</li>
+        <li><b>Cell Phone Number:</b> ${userObj.cell}</li>
     </ul>
     </section>`
 }
--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,13 @@
 import {getUsers} from"./DataManager.js"
 import {addUserToDom} from"./DataList.js"
 
+const userBtn = document.querySelector(".user-btn")
 getUsers().then(parsedResponse => {
-    
     addUserToDom(parsedResponse.results[0]);
 })
-
+    userBtn.addEventListener("click", taco => {
+        getUsers().then(parsedResponse => {
+            addUserToDom(parsedResponse.results[0]);
+})
+    })
 console.log("You look marvelous!")
